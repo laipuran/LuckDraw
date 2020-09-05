2131fb894538d39feec91cb657aaf85bc6a2f754
laipuran <55612059+laipuran@users.noreply.github.com>
2020-09-05 21:17:04 +0800

    Submitted to Windows Store

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Data.Json;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
