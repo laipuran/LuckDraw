dfe94623c64038cb9fcfc04996b0aecd0aea6e5d
laipuran <55612059+laipuran@users.noreply.github.com>
2020-10-06 13:13:32 +0800

    Can save last numbers and fixed some bugs

--- LuckDrawWindow/LuckDrawPage.xaml.cs
+++ LuckDrawWindow/LuckDrawPage.xaml.cs
@@ -23,6 +23,83 @@ namespace LuckDrawWindow
         public LuckDrawPage()
         {
             InitializeComponent();
+
+            ResultTextBlock.Text = Properties.Settings.Default.numbersLastTime;
+        }
+        static public int getRand(int a, int b)
+        {
+            Random r = new Random();
+            int num = r.Next(a, b);
+            return num;
+        }
+
+        private void GetNumberButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            NumberTextBox.IsEnabled = false;
+            GetNumberButton.IsEnabled = false;
+
+            int number = int.Parse(NumberTextBox.Text);
+            if (number <= 0)
+            {
+                ResultTextBlock.Text = "输入值非法！";
+                GetNumberButton.IsEnabled = true;
+                NumberTextBox.IsEnabled = true;
+                return;
+            }
+            int max = App.numberOfPeople;
+
+            int[] array = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                array[i] = getRand(1, max + 1);
+            }
+
+            int[] rankedNumbers = new int[number];
+            bool flag = true;
+            for (int i = 0; i < number - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                for (int i = number - 1; i > 0; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp;
+                            temp = array[j + 1];
+                            array[j + 1] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+
+            string output;
+            output = "被抽中的幸运同学：\n" + string.Join("\n", array);
+            Properties.Settings.Default.numbersLastTime = output;
+            ResultTextBlock.Text = output;
+
+            if (App.doShowToasts)
+            {
+                string notifications;
+                notifications = "被抽中的幸运同学： " + string.Join(" ", array);
+            }
+
+
+            GetNumberButton.IsEnabled = true;
+            NumberTextBox.IsEnabled = true;
         }
     }
 }
